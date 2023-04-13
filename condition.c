ec32ea124175c137807863ffc3bfa1445017b57c
30805 <129129763+ehrudtn@users.noreply.github.com>
2023-04-13 01:26:22 +0000

    홀짝게임

--- condition.c
+++ condition.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-main(){
-  int a=5, b=3;
+// #include <stdio.h>
+// main(){
+//   int a=5, b=3;
 
-  if(5>3){              //참, if 뒤에 들여쓰기
-  printf("A\n");
-  }    
+//   if(5>3){              //참, if 뒤에 들여쓰기
+//   printf("A\n");
+//   }    
   
-  printf("B\n");
+//   printf("B\n");
+  
+//   if(5+3) {
+//     printf("C\n");
+//   }
+//     printf("D\n");
 
-  if(5+3) {
-    printf("C\n");
-  }
-    printf("D\n");
+//   if(5-5) {
+//     printf("E\n");  //거짓 5-5=0
+//   }
+//   if(a>b) {
+//     printf("F\n");
+//   }
+//   printf("G\n");
 
-  if(5-5) {
-    printf("E\n");  //거짓 5-5=0
-  }
-  if(a>b) {
-    printf("F\n");
-  }
-  printf("G\n");
-
-  if(a<b) {
-    printf("H\n");
-  }
-  printf("I\n");
+//   if(a<b) {
+//     printf("H\n");
+//   }
+//   printf("I\n");
   
   
-}
+// }
