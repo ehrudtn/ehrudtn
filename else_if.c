9b2ff1d8dd630cf35cbaa0112178695212e1b70b
ehrudtn <129129763+ehrudtn@users.noreply.github.com>
2023-04-18 01:12:52 +0000

    condition_elseif.c

---
+++ else_if.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main(){
+  int x;
+  printf("1번 사과 2번 바나나 3번 두리안 4번 파파야\n");
+  scanf("%d", &x);
+  if(x==1)
+    printf("사과를 좋아하는군요!");
+  else if(x==2)
+    printf("바나나를 좋아하는군요!");
+  else if(x==3)
+    printf("두리안을 좋아하시는군요!");
+  else if(x==4)
+    printf("파파야를 좋아하시는군요!");
+  else
+    printf("당신이 좋아하는 과일이 없군요!");
+  printf("좋아하는 과일 조사 끝");
+  
+}
